655cc7ccfb91d20239d49ad66ccd0eb7e9a63286
UNBunny <nikitakuharchuk07@gmail.com>
2025-02-12 02:57:02 +0600

    - new branch

--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -1,64 +1,49 @@
 package com.student.controller;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import com.student.StudentProperties;
+import jakarta.inject.Inject;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.student.core.Student;
 import com.student.service.StudentService;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.lang.NonNull;
-import org.springframework.web.bind.annotation.*;
 
-import javax.inject.Inject;
-import java.net.URI;
-import java.util.Collection;
-import java.util.Optional;
+ 
 
-@CrossOrigin
-@RestController
 @RequestMapping("/student")
+@RestController
+@CrossOrigin
 public class StudentController {
-    @Inject
-    private StudentProperties studentProperties;
-
-    @Inject
-    private StudentService studentService;
-
-
-    @GetMapping("/msg")
-    public String getMessage(@RequestHeader("user-agent") String userAgent) {
-        return userAgent;
-    }
-
-    @GetMapping
-    public Collection<Student> getAll() {
-        return studentService.getAllStudents();
-    }
-
-    @GetMapping("/{id}")
-    public Student getStudent(@PathVariable long id) {
-        return studentService.get(id);
-    }
-
-    @GetMapping(path = "/single", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<Student> getSingleStudent(@RequestParam("id") Optional<Long> optional) {
-        return ResponseEntity.ok(studentService.get(optional.orElse(1L)));
-    }
-
-    @GetMapping("/search/{department}")
-    public Collection<Student> getStudentsPerDepartment(@PathVariable String department, @RequestParam("name") Optional<String> optional) {
-        return studentService.getAllStudentsInDepartment(department, optional.orElse(""));
-    }
-
-    @PostMapping
-    public ResponseEntity<String> addStudent(@RequestBody Student student) {
-        studentService.add(student);
-        if (student.getId() > 0) {
-            URI uri = URI.create("/college/student/" + student.getId());
-            System.out.println(uri);
-            return ResponseEntity.accepted().location(uri).build();
-        } else {
-            return ResponseEntity.badRequest().build();
-        }
-    }
+	
+	 
+	@Inject
+	private StudentService studentService;
+ 
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public Collection<Student> getAll() {
+		return studentService.getAllStudents();
+	}
+	
+	@GetMapping(path="/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Student getStudent(@PathVariable("id") long id) {
+		return studentService.get(id);
+	}
+	
+	@GetMapping(path="/search/department", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<String> getDepartments() {
+		 return studentService.getAllStudents().stream().map(p-> p.getDept()).distinct().collect(Collectors.toList());
+	}
+	
+ 
+	 
 }
