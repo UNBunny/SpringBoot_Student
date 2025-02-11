b0eb6802b992c6cbe323c8ea04934bdd7ec3c09c
UNBunny <nikitakuharchuk07@gmail.com>
2025-02-12 02:58:46 +0600

    - transition to spring boot 3

---
+++ src/main/java/com/student/service/StudentServiceImpl.java
@@ -0,0 +1,27 @@
+package com.student.service;
+
+import com.student.core.Student;
+
+import java.util.Collection;
+
+import jakarta.inject.Named;
+
+@Named
+public class StudentServiceImpl implements StudentService {
+
+    //TODO
+
+    @Override
+    public Student get(long id) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Collection<Student> getAllStudents() {
+        //TODO
+        return null;
+    }
+
+
+}
